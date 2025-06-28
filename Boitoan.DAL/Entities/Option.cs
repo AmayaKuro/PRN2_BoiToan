a422f322993605e4aa768f9797d271ec31bc8fb8
AmayaKuro <vhtkiet9615@gmail.com>
2025-06-28 20:52:01 +0700

    add test generator (not used yet)

--- Boitoan.DAL/Entities/Option.cs
+++ Boitoan.DAL/Entities/Option.cs
@@ -4,5 +4,5 @@ public class Option
 {
     public int Id { get; set; } 
     public string Detail { get; set; }
-    public string Value { get; set; } // e.g., "M|B|T", "D|T|B|I" 
+    public AllAnswer Value { get; set; } // e.g., "M|B|T", "D|T|B|I" 
 }
